88180b4054396013260de1707ad00de69cec3673
dimpablo <dimpablo@yandex.ru>
2025-08-18 18:16:54 +0300

    auto: update 2025-08-18 18:16

--- lesson3/sw.js
+++
@@ -1,8 +0,0 @@
-// Service Worker для урока 3
-self.addEventListener('install', (event) => {
-  console.log('Service Worker установлен для урока 3');
-});
-
-self.addEventListener('activate', (event) => {
-  console.log('Service Worker активирован для урока 3');
-});
